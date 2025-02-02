fc0d4f46de65effc8bae7735d8dc78fb1aafb2d2
mahmud <mahmudhussaininfo@gmail.com>
2025-02-02 11:47:51 +0600

    hello

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import colors from "colors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.get("/help", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`.bgGreen.bold);
+});
